f59279e29b0311e2525dca77185d7dade23537de
TeylorT <tippinsteylor@gmail.com>
2024-06-17 17:35:43 -0500

    test

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "30a82833237af062fb893c7bd644d681"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "61f511af17944364b39a6d6e1693f161"
